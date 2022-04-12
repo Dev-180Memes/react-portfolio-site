f97b12039f9e76fce1d240b9ef2d31f0e11a4327
Dev-180Memes <adeoluwaagbakosi@gmail.com>
2022-04-12 14:10:52 +0000

    First Commit

---
+++ backend_sanity/schemas/testimonials.js
@@ -0,0 +1,17 @@
+export default {
+	name: 'testimonials',
+	title: 'Testimonials',
+	type: 'document',
+	fields: [
+		{
+			name: 'name',
+			title: 'Name',
+			type: 'string'
+		},
+		{
+			name: 'feedback',
+			title: 'Feedback',
+			type: 'string'
+		},
+	]
+}
