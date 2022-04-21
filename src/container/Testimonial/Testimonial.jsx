4cbc31aa19de22ae1e04ca9766783dc529226386
Dev-180Memes <adeoluwaagbakosi@gmail.com>
2022-04-21 17:48:22 +0000

    Minor bug fix

--- src/container/Testimonial/Testimonial.jsx
+++ src/container/Testimonial/Testimonial.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { HiChevronLeft, HiChevronRight } from 'react-icons/hi';
-// import { motion } from 'framer-motion';
 
 import { AppWrap, MotionWrap } from '../../wrapper';
 import { client } from '../../client';
@@ -9,22 +8,16 @@ import './Testimonial.scss';
 const Testimonial = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [testimonials, setTestimonials] = useState([]);
-  const [brands, setBrands] = useState([]);
-
+  
   const handleClick = (index) => {
     setCurrentIndex(index);
   };
 
   useEffect(() => {
     const query = '*[_type == "testimonials"]';
-    const brandsQuery = '*[_type == "brands"]';
 
     client.fetch(query).then((data) => {
       setTestimonials(data);
-    });
-
-    client.fetch(brandsQuery).then((data) => {
-      setBrands(data);
     });
   }, []);
 
@@ -52,18 +45,6 @@ const Testimonial = () => {
           </div>
         </>
       )}
-
-      {/* <div className="app__testimonial-brands app__flex">
-        {brands.map((brand) => (
-          <motion.div
-            whileInView={{ opacity: [0, 1] }}
-            transition={{ duration: 0.5, type: 'tween' }}
-            key={brand._id}
-          >
-            <img src={urlFor(brand.imgUrl)} alt={brand.name} />
-          </motion.div>
-        ))}
-      </div> */}
     </>
   );
 };
